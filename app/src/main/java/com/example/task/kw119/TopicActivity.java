8da9a147ebc799923fdc58a0315a8ae855f9a634
ghdud4006 <ghdud4006@gmail.com>
2018-06-19 21:51:38 +0900

    Update MyTopicActivity

--- app/src/main/java/com/example/task/kw119/TopicActivity.java
+++ app/src/main/java/com/example/task/kw119/TopicActivity.java
@@ -1,28 +1,68 @@
 package com.example.task.kw119;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class TopicActivity extends AppCompatActivity {
 
     private static final String TAG="KW119-MyList";
     private static final String GET_TOPIC_URL_ADDRESS="http://13.125.217.245:3000/topic";
 
+
+    // use for server comm
     private String mServerMsg;
     private int mUserSessionId;
+    private int mTopicNum;
+
+
+    private String mTitle, mKind, mLocation, mContents;
+
+
+
+    private TextView mTvTitle, mTvKind, mTvLocation, mTvContents;
+    private ImageView mIvImg;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topic);
 
-        setTitle("타이틀");
-
+        // initiallize activity
         Intent intent = getIntent();
         mUserSessionId = intent.getExtras().getInt("sid");
+        mTopicNum = intent.getExtras().getInt("topic_num");
+        mTitle = intent.getStringExtra("topic_title");
+
+        setTitle(mTitle);
+
+
+
 
         // 액션바 뒤로가기 버튼 추가
         ActionBar actionBar = getSupportActionBar();
@@ -48,5 +88,104 @@ public class TopicActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
         startActivity(intent);
         finish();
+    }
+
+
+    private void mOnClick(View v){
+        switch (v.getId()){
+            case R.id.btnTopicUpdate:
+
+                break;
+            case R.id.btnTopicCancel:
+                finish();
+                break;
+
+        }
+    }
+
+    public class ConnServerAsyncTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            mServerMsg=null;
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.accumulate("sid", mUserSessionId);
+                jsonObject.accumulate("topicNumber", mTopicNum);
+
+
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
+
+                try{
+                    URL url = new URL(urls[0]);
+                    con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("POST");
+                    con.setRequestProperty("Cache-Control", "no-cache");
+                    con.setRequestProperty("Content-Type", "application/json");
+                    con.setRequestProperty("Accept", "text/html");
+                    con.setDoOutput(true);
+                    con.setDoInput(true);
+                    con.connect();
+
+                    OutputStream outStream = con.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
+                    writer.write(jsonObject.toString());
+                    writer.flush();
+                    writer.close();
+
+                    InputStream stream = con.getInputStream();
+
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    StringBuffer buffer = new StringBuffer();
+
+                    String line = "";
+                    while((line = reader.readLine()) != null){
+                        buffer.append(line);
+                    }
+                    mServerMsg = buffer.toString();
+                    Log.v(TAG, "receive data from server");
+                    return mServerMsg;
+                } catch (MalformedURLException e){
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if(con != null){
+                        con.disconnect();
+                    }
+                    try {
+                        if(reader != null){
+                            reader.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if(mServerMsg.equals("err")){
+                Toast.makeText(getApplicationContext(), mServerMsg, Toast.LENGTH_SHORT).show();
+                finish();
+            } else { // result
+
+            }
+        }
+
     }
 }
